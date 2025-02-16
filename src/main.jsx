bdba94405f59efde6c8d81f80ac8c238ab451505
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-02-16 20:33:24 +0800

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App1 from "./App-v1.jsx";
+
+// import StarRating from "./StarRating";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App1 />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} /> */}
+  </StrictMode>
+);
